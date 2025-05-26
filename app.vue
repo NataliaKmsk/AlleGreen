61ebc80e86135bb245271ecdb9fc9ef92f119708
NataliaKmsk <nataliak289@gmail.com>
2025-05-26 15:18:00 +0200

    Inicjalizacja

---
+++ app.vue
@@ -0,0 +1,16 @@
+<template>
+  <ul>
+    <li>
+      <NuxtLink to="/">Strona glowna</NuxtLink>
+    </li>
+    <li>
+      <NuxtLink to="/about">O nas</NuxtLink>
+    </li>
+  </ul>
+  <br />
+
+  <NuxtPage />
+</template>
+<script setup>
+
+</script>
---
+++ pages/cart.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <span>strona o nas</span>
+    </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style></style>
